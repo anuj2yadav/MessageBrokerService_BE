dc15b8bfa2b5a3bdd19e2063e83d38505027340b
anujyadav <anujyadav1441998@gmail.com>
2024-12-22 19:09:15 +0530

    create application

---
+++ messagPublicationService/src/main/java/com/anuj/messagPublicationService/MessagPublicationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.anuj.messagPublicationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagPublicationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagPublicationServiceApplication.class, args);
+	}
+
+}
